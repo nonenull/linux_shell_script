0310b97f522455fb276ab7ec5cc99d7a01ffece7
Ken.Chen <316084217@qq.com>
2020-04-27 21:25:50 +0800

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -26,3 +26,8 @@ cat > /etc/docker/deamon.json <<EOF
 EOF
 
 systemctl enable docker && systemctl restart docker
+
+
+curl -L https://github.com/docker/compose/releases/download/1.17.1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+curl -L https://raw.githubusercontent.com/docker/compose/1.8.0/contrib/completion/bash/docker-compose > /etc/bash_completion.d/docker-compose
